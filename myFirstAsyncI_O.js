85cf2d93f31bc3e90df8759c5278148deb3df172
harish <harishn@gmx.com>
2015-07-16 22:22:08 +0530

    Completed exercises 1-4

--- myFirstAsyncI_O.js
+++ myFirstAsyncI_O.js
@@ -3,21 +3,22 @@ var filename = process.argv[2];
 
 // Approach using readFile - whole file is read
 
-// fs.readFile(filename, {encoding:"utf8"}, function(error, data) {
-// 	if (error) {
-// 		console.error(error.message);
-// 	} else {
-// 		console.log(data.split("\n").length - 1);
-// 	}
-// });
+fs.readFile(filename, {encoding:"utf8"}, function(error, data) {
+	if (error) {
+		console.error(error.message);
+	} else {
+		console.log(data.split("\n").length - 1);
+	}
+});
 
 // Approach using streams - chunks of varying sizes of file are read
+// Note: Streams are unstable library, yet as on Jul 16 2015, as per nodejs.org
 
-var readStream = fs.createReadStream(filename, {encoding: "utf8"});
-var fileContents = "";
+// var readStream = fs.createReadStream(filename, {encoding: "utf8"});
+// var fileContents = "";
 
-readStream.on("data", function(chunk) {
-	fileContents += chunk;
-}).on("end", function() {
-	console.log(fileContents.split("\n").length - 1);
-});
+// readStream.on("data", function(chunk) {
+// 	fileContents += chunk;
+// }).on("end", function() {
+// 	console.log(fileContents.split("\n").length - 1);
+// });
